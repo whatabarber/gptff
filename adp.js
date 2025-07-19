e0c975acfd8d06da03801f77ed82ada2286afb62
whatabarber <77034235+whatabarber@users.noreply.github.com>
2025-07-18 21:44:42 -0500

    Delete adp.js

--- adp.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const table = document.getElementById('adp-table');
-    table.innerText = 'Loading ADP rankings... (Static preview)';
-});
